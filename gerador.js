cb07fe09df00e5ca3e64107a6447a11065035ad7
iOrlly <coelhoabnt2@gmail.com>
2025-09-08 14:14:27 -0300

    projeto citação
    adicionando mais algumas citações e CSS e finalizando de mais.

--- gerador.js
+++ gerador.js
@@ -14,15 +14,36 @@ const citacoes = [
     {
         texto: "Sem música, a vida seria um erro.",
         autor: "Friedrich Nietzsche",
+    },
+    {
+        texto: "A persistência é o caminho do êxito.",
+        autor: "Charles Chaplin"
+    },
+    {
+        texto: "O único lugar onde o sucesso vem antes do trabalho é no dicionário.",
+        autor: "Albert Einstein"
+    },
+    {
+        texto: "Acredite que você pode, assim você já está no meio do caminho.",
+        autor: "Theodore Roosevelt"
+    },
+    {
+        texto: "Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.",
+        autor: "Roberto Shinyashiki"
+    },
+    {
+        texto: "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+        autor: "Robert Collier"
     }
 
 ];
+console.log(citacoes.length)
 
 function gerarCitacao() {
     //const textoCitacoes = document.getElementById('citacao')
-    const gerar = Math.floor(Math.random() * citacoes.length);
+    const gerar = Math.floor(Math.random() * citacoes.length); //informa o tamanho do arrey
 
-    const citacaoSorteada = citacoes[gerar];
+    const citacaoSorteada = citacoes[gerar]; // = citacoes[gerar]; é o caminho que a variável citacaoSorteada vai percorer para pegar o objeto
 
     document.getElementById('textoCitacao').innerHTML = `"${citacaoSorteada.texto}"`;
     document.getElementById('autor').innerHTML = `-${citacaoSorteada.autor}`;
