1821272341f74448be2c0b2d48bfa644fd446f49
iOrlly <coelhoabnt2@gmail.com>
2025-09-08 11:00:59 -0300

    projeto citação
    estrutura HTML e JS base criadas.

---
+++ gerador.js
@@ -0,0 +1,24 @@
+const citacoes = [
+        {
+        texto: "Aquilo que não me mata, fortalece-me.",
+        autor: "Friedrich Nietzsche",
+    },
+    {
+        texto: "Aquele que luta com monstros deve acautelar-se para não se tornar também um monstro. E se tu olhares durante muito tempo para um abismo, o abismo também olha para dentro de ti.",
+        autor:"Friedrich Nietzsche",
+    },
+    {
+        texto: "Deus está morto! E nós o matamos!",
+        autor: "Friedrich Nietzsche",
+    },
+    {
+        texto: "Sem música, a vida seria um erro.",
+        autor: "Friedrich Nietzsche",
+    }
+
+];
+
+function generation() {
+    const textoCitacoes = getElementById('citacao')
+    const nomeAutor = getElementById('autor')
+}
